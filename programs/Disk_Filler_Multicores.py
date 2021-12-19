cbfe23c46624f2ee3a285bc5e5fda420c41f252c
Shreyansh Kuntal <45984673+shreykuntal@users.noreply.github.com>
2021-12-19 21:41:07 +0530

    Add files via upload

---
+++ programs/Disk_Filler_Multicores.py
@@ -0,0 +1,44 @@
+from multiprocessing import Process
+import random
+def fill_space1():
+    name = 'file' + str(random.randint(0,1000)) + '.txt'
+    f = open(name,'w')
+    for i in range(1000000000000):
+        f.write("I am He-Who-Must-Not-Be-Named ")
+    f.close()
+def fill_space2():
+    name = 'file' + str(random.randint(0,1000)) + '.txt'
+    f = open(name,'w')
+    for i in range(1000000000000):
+        f.write("I am He-Who-Must-Not-Be-Named ")
+    f.close()
+def fill_space3():
+    name = 'file' + str(random.randint(0,1000)) + '.txt'
+    f = open(name,'w')
+    for i in range(1000000000000):
+        f.write("I am He-Who-Must-Not-Be-Named ")
+    f.close()
+def fill_space4():
+    name = 'file' + str(random.randint(0,1000)) + '.txt'
+    f = open(name,'w')
+    for i in range(1000000000000):
+        f.write("I am He-Who-Must-Not-Be-Named ")
+    f.close()
+fill_space1()
+def Main():
+    p1 = Process(target=fill_space1)
+    p2 = Process(target=fill_space2)
+    p3 = Process(target=fill_space3)
+    p4 = Process(target=fill_space4)
+    
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+    
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+if __name__=='__main__':
+    Main()
